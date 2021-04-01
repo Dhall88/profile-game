ad07e1a4b6fc6e167b04693abeb959112dbc49df
David Hall <davidchall388@gmail.com>
2021-04-01 13:44:50 -0400

    controls working, constant speed in x,z plane regards of camera angles

--- src/components/Controls/controls.js
+++ src/components/Controls/controls.js
@@ -91,7 +91,7 @@
 
 // // export default Controls;
 
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { extend, useFrame, useThree } from "@react-three/fiber";
 // import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls";
 import { PointerLockControls, PerspectiveCamera} from "@react-three/drei";
@@ -99,18 +99,86 @@ import { PointerLockControls, PerspectiveCamera} from "@react-three/drei";
 // extend({ PointerLockControls });
 
 const Controls = props => {
-  const camera = useRef();
-  useFrame(() => {
-      console.log(camera)
-  })
-//   const {
-//     camera,
-//     gl: { domElement }
-//   } = useThree();
-//   useFrame(() => ref.current && ref.current.update());
+
+//   const camera = useRef();
+
+      function useKeyPress(targetKey) {
+        // State for keeping track of whether key is pressed
+        const [keyPressed, setKeyPressed] = useState(false);
+      
+        // If pressed key is our target key then set to true
+        function downHandler({ key }) {
+            console.log(key)
+          if (key === targetKey) {
+            setKeyPressed(true)
+          }
+        }
+      
+        // If released key is our target key then set to false
+        const upHandler = ({ key }) => {
+          if (key === targetKey) {
+            setKeyPressed(false)
+          }
+        };
+      
+        // Add event listeners
+        useEffect(() => {
+          window.addEventListener('keydown', downHandler);
+          window.addEventListener('keyup', upHandler);
+          // Remove event listeners on cleanup
+          return () => {
+            window.removeEventListener('keydown', downHandler);
+            window.removeEventListener('keyup', upHandler);
+          };
+        }, []); // Empty array ensures that effect is only run on mount and unmount
+      
+        return keyPressed;
+      }
+
+      const forward = useKeyPress('w');
+      const right = useKeyPress('a');
+      const backward = useKeyPress('s');
+      const left = useKeyPress('d');
+
+//     useFrame(() => {
+//         // camera.current.rotation._y=Math.PI/2
+        // if(forward) {
+        //     camera.current.translateZ(-.01)
+        // } if(backward){
+        //     camera.current.translateZ(.01)
+        // } if(right){
+        //     camera.current.translateX(-.01)
+        // } if(left){
+        //     camera.current.translateX(.01)
+        // }
+
+        // console.log(camera.current)
+//     })
+
+const {
+    camera
+  } = useThree();
+
+  const speed = .1
+useFrame(()=> {
+              if(forward) {
+            camera.translateZ(-speed);
+            camera.translateY(speed)
+        } if(backward){
+            camera.translateZ(speed)
+            camera.translateY(-speed)
+        } if(right){
+            camera.translateX(-speed)
+        } if(left){
+            camera.translateX(speed)
+        }
+
+        camera.position.y=5;
+        console.log(camera.rotation)
+})
   return <>
-  <PerspectiveCamera makeDefault ref={camera} position={[0,3,0]}/>
-  <PointerLockControls camera={camera.current} />
+  {/* <PerspectiveCamera makeDefault ref={camera} position={[0,3,0]}/> */}
+  <PointerLockControls />
   </>
 };
 
